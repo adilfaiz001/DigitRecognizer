ae5c899c78aedb421dc37d7a9405db746eeb70fe
adilfaiz01 <adilfaiz01@live.com>
2018-04-18 04:44:25 +0530

    build neural net for digit recognition

---
+++ load_data.py
@@ -0,0 +1,33 @@
+'''
+Created on Apr 13, 2018
+
+@author: adil
+'''
+import numpy as np 
+import gzip
+import pickle
+
+
+def load_data():
+    with gzip.open('mnist.pkl.gz','rb') as f:
+        tr_d, va_d, te_d =pickle.load(f)
+    
+    train_x,train_y=tr_d
+    train_y=np.array(np.squeeze([vectorized_result(y) for y in train_y],axis=0)) 
+        
+    training_data=(train_x,train_y)   
+    validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
+    validation_data = zip(validation_inputs, va_d[1])
+    test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
+    test_data = zip(test_inputs, te_d[1])
+    return (training_data,validation_data,test_data)
+    
+def vectorized_result(j):
+    """Return a 10-dimensional unit vector with a 1.0 in the jth
+    position and zeroes elsewhere.  This is used to convert a digit
+    (0...9) into a corresponding desired output from the neural
+    network."""
+    e = np.zeros((10, 1))
+    e[j] = 1.0
+    return e
+
---
+++ train.py
@@ -0,0 +1,19 @@
+'''
+Created on Apr 16, 2018
+
+@author: adil
+'''
+import numpy as np 
+from load_data import load_data
+
+train_set,valid_set,test_set=load_data()
+
+'''
+training set as X ,Y
+'''
+x,y=train_set
+
+
+
+
+
